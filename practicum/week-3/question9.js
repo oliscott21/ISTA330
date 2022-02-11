705af20869acc741f846532c850b9d629ee8ad05
oliscott21 <oliscott21@gmail.com>
2022-02-11 15:19:29 -0700

    question 9 done

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -1,7 +1,7 @@
 /*
-Given an integer n, 
-put each number from 1 to n 
-into groups based on the sum of its digits. 
+Given an integer n,
+put each number from 1 to n
+into groups based on the sum of its digits.
 
 Return how many groups have the largest size.
 
@@ -14,4 +14,35 @@ so there are two groups with the largest size.
 
 var largestGroupsCount = function(n) {
 
+  let groups = [];
+
+  for (let i = 1; i <= n; i++) {
+    let cur = [];
+    for (let j = 1; j <= n; j++) {
+      let temp = j;
+      let val = 0;
+      while (temp) {
+        val += temp % 10;
+        temp = temp / 10 | 0;
+      }
+      if (val == i) {
+        cur.push(j);
+      }
+    }
+    groups.push(cur);
+  }
+
+  let count = 0;
+  let max = 0;
+  for (let i = 0; i < groups.length; i++) {
+    if (groups[i].length == max) {
+      count += 1;
+    }
+    if (groups[i].length > max) {
+      count = 0;
+      max = groups[i].length;
+      count += 1;
+    }
+  }
+  return count;
 };
